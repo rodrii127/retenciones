2e155bc47c5ec9d26f62ef632aee533ef83cbbcc
rodrii127 <44079303+rodrii127@users.noreply.github.com>
2022-03-08 19:15:34 -0300

    first commit

---
+++ retenciones/src/main/java/com/sevenb/retenciones/RetencionesApplication.java
@@ -0,0 +1,13 @@
+package com.sevenb.retenciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetencionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetencionesApplication.class, args);
+	}
+
+}
