1a17e485ac4c18b6ce54aabf4067cc92bad09434
Rodrigo Rodriguez <rodrigo.rodriguez@valtech.com>
2022-04-19 23:04:30 -0300

    create method to search invoice by specific dates

--- retenciones/src/main/java/com/sevenb/retenciones/controller/implementation/InvoiceController.java
+++ retenciones/src/main/java/com/sevenb/retenciones/controller/implementation/InvoiceController.java
@@ -1,47 +1,48 @@
 package com.sevenb.retenciones.controller.implementation;
 
-import com.sevenb.retenciones.entity.Invoice;
-import com.sevenb.retenciones.service.definition.InvoiceService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.sevenb.retenciones.entity.Invoice;
+import com.sevenb.retenciones.entity.SearchDate;
+import com.sevenb.retenciones.service.definition.InvoiceService;
 
 @RestController
-@RequestMapping(value ="/invoice", produces = "application/json")
+@RequestMapping(value = "/invoice", produces = "application/json")
 class InvoiceController {
 
     @Autowired
     private InvoiceService invoiceService;
 
     @PostMapping(produces = "application/json")
-    public Invoice save(@RequestBody Invoice invoice){
+    public Invoice save(@RequestBody Invoice invoice) {
         return invoiceService.save(invoice);
     }
 
     @GetMapping
-    public List<Invoice> invoice(){
+    public List<Invoice> invoice() {
         return invoiceService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Invoice findInvoiceId(@PathVariable Long id){
-                return invoiceService.findById(id);
+    public Invoice findInvoiceId(@PathVariable Long id) {
+        return invoiceService.findById(id);
     }
 
     @PutMapping("/{id}")
-    public Invoice update(@RequestBody Invoice invoice, @PathVariable Long id ) {
-        return invoiceService.update( invoice,id);
+    public Invoice update(@RequestBody Invoice invoice, @PathVariable Long id) {
+        return invoiceService.update(invoice, id);
     }
 
-  /*  @GetMapping
-    public Optional<Invoice> findByDateBetween(LocalDate date1, LocalDate date2){
-
-        return null;
-    }*/
-
-
-
+    @GetMapping("/date")
+    public List<Invoice> findByDateBetween(@RequestBody SearchDate searchDate) {
+        return invoiceService.findByDateBetween(searchDate.getStartDate(), searchDate.getEndDate());
+    }
 }
